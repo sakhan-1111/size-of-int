1e0fbca25039b793fadde54f796884c907497011
Shafiqul Alam Khan <118207040+sakhan-1111@users.noreply.github.com>
2023-12-19 16:53:39 -0600

    Create size of int.c

---
+++ size of int.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    printf("The size of Int = %d\n", sizeof(int));
+    printf("The size of Float = %d\n", sizeof(float));
+    printf("The size of Double = %d\n", sizeof(double));
+    printf("The size of Long Double = %d\n", sizeof(long double));
+    printf("The size of Character = %d", sizeof(char));
+    return 0;
+}
